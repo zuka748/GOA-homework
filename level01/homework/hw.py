542f38a44a95f32e411ecadc92a45c2e0e21d861
zuka748 <zzuka2881@gmail.com>
2025-02-19 18:57:40 +0300

    first commit

---
+++ level01/homework/hw.py
@@ -0,0 +1,5 @@
+name=("zuka") #string სტრინგი არის ბრჭყალებში მოგცეული ტექსტი
+surname=("bichoshvili")
+age=(15)  #integer მთელი რიცხვები
+heigh=(175.5) #float ათ წილადი რიცხვები
+print(name+surname+str(age)+str(heigh))
