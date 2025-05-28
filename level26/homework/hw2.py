dca02d0433b6d100107cf6c30547d7c505f0dac8
zuka748 <zzuka2881@gmail.com>
2025-05-29 00:43:19 +0300

    commit

--- level26/homework/hw2.py
+++ level26/homework/hw2.py
@@ -1,6 +1,22 @@
 # 3. შექმენით ფუნქცია რომელიც არგუმენტად მიიღებს ლისტს და გამოიტანეთ ის data type რომელიც ყველაზე ხშირად გვხვდება ამ ლისტსში.
 
 def lstn(lst1):
-    type_count=[]
+    ints=0
+    strs=0
+    floats=0
     for i in lst1:
+        if type(i)==int:
+            ints+=1
+        elif type(i)==str:
+            strs+=1
+        elif type(i)==float:
+            floats+=1
+
+    if ints>strs and ints>floats:
+        return int
+    elif strs>ints and strs>floats:
+        return str
+    else:
+        return float
+
         
